9e4b34ed952f8d5bebdf8c8765bd65fe99d2fdb1
lijialiang93 <lijialiang93@gmail.com>
2017-09-28 00:46:39 -0400

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,172 @@
+interval = 120;
+px = 15;
+py = 10;
+tcw = 30;
+tch = 20;
+gs = 20;
+ax = 20;
+ay = 15;
+xv = yv = 0;
+trail = [];
+tail = 4;
+score = 0
+high = 0;
+prev = 0;
+var pointSound = document.createElement("audio");
+var deathSound = document.createElement("audio");
+var highSound = document.createElement("audio");
+
+pointSound.src = "media/point.wav";
+deathSound.src = "media/death.wav";
+highSound.src = "media/high.wav";
+
+pointSound.preload = "auto";
+deathSound.preload = "auto";
+highSound.preload = "auto";
+
+window.onload = function() {
+	canv = document.getElementById("gc");
+	ctx = canv.getContext("2d");
+	document.addEventListener("keydown", keyPush);
+	run = setInterval(game, interval);
+}
+
+function game() {
+	clearInterval(run);
+	px += xv;
+	py += yv;
+
+	if (px < 0) {
+		px = tcw - 1;
+	}
+	if (px > tcw - 1) {
+		px = 0;
+	}
+	if (py < 0) {
+		py = tch - 1;
+	}
+	if (py > tch - 1) {
+		py = 0;
+	}
+
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, canv.width, canv.height);
+
+	ctx.fillStyle = "red";
+	ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
+
+	ctx.fillStyle = "black";
+	for (var i = 0; i < trail.length; i++) {
+
+		ctx.fillRect(trail[i].x * gs, trail[i].y * gs, gs - 2, gs - 2);
+
+		if (i == trail.length - 1) {
+			ctx.clearRect(trail[i].x * gs + 4, trail[i].y * gs + 4, gs / 2, gs / 2);
+		}
+
+		if (trail[i].x == px && trail[i].y == py) {
+			if (score > 0) {
+				deathSound.play();
+			}
+			prev = high;
+			tail = 4;
+			score = 0;
+			interval = 120;
+		}
+	}
+
+	ctx.font = "15px Verdana";
+	ctx.fillStyle = "red";
+	ctx.fillText(high, 15, 15);
+	ctx.fillStyle = "black";
+	ctx.fillText(score, 15, 30);
+
+
+	trail.push({
+		x: px,
+		y: py
+	});
+
+	while (trail.length > tail) {
+		trail.shift();
+	}
+
+	if (ax == px && ay == py) {
+		score++
+		if (score > prev && prev != 0) {
+			highSound.play();
+			prev = 9999;
+		} else {
+			pointSound.play();
+		}
+		if (score > high) {
+			high = score;
+		}
+
+		tail++;
+		interval = interval * 0.98;
+
+		loop:
+			while (true) {
+				ax = Math.floor(Math.random() * tcw);
+				ay = Math.floor(Math.random() * tch);
+				for (var i = 0; i < trail.length; i++) {
+					if (trail[i].x == ax && trail[i].y == ay) {
+						continue loop;
+					}
+				}
+				break;
+			}
+
+		run = setInterval(game, interval);
+		return;
+	}
+	run = setInterval(game, interval);
+	return;
+
+}
+
+
+function keyPush(evt) {
+
+	if (evt.keyCode == 37 || evt.keyCode == 65) {
+		if (xv != 1) {
+			xv = -1;
+			yv = 0;
+		}
+	} else if (evt.keyCode == 38 || evt.keyCode == 87) {
+		if (yv != 1) {
+			xv = 0;
+			yv = -1;
+		}
+	} else if (evt.keyCode == 39 || evt.keyCode == 68) {
+		if (xv != -1) {
+			xv = 1;
+			yv = 0;
+		}
+
+	} else if (evt.keyCode == 40 || evt.keyCode == 83) {
+		if (yv != -1) {
+			xv = 0;
+			yv = 1;
+		}
+	}
+}
+
+function mute() {
+	var muteBtn = document.getElementById("muteBtn");
+
+	if (muteBtn.value === "MUTE") {
+		muteBtn.src = "images/off.png"
+		muteBtn.value = "UNMUTE";
+		pointSound.muted = true;
+		deathSound.muted = true;
+		highSound.muted = true;
+	} else {
+		muteBtn.src = "images/on.png"
+		muteBtn.value = "MUTE";
+		pointSound.muted = false;
+		deathSound.muted = false;
+		highSound.muted = false;
+	}
+}
